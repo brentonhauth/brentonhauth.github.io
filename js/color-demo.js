46a697498d1c7f9184b56e17b87dc4c3f51d94c8
brenton-h <37673694+brenton-h@users.noreply.github.com>
2018-11-21 18:30:35 -0500

    Overhaul

---
+++ js/color-demo.js
@@ -0,0 +1,98 @@
+(function() {
+
+    $("input.R2G").on("input", function() {
+        var clr = jsrc.color.redToGreenRatio($(this).val() / 100, false);
+        let fc = "rgba("+clr[0]+","+clr[1]+","+clr[2]+","+0.67+")";
+        $("div.R2G").css("background-color", fc);
+    });
+
+    function checkBrightness(R, G, B) {
+        R /= 255; G /= 255; B /= 255;
+        var x = Math.max(R,G,B) + Math.min(R,G,B);
+        return x / 2;
+    }
+
+    let lst = $("input.rgbMixer.color-1").get();
+
+    var rgbMixColor1 = new LinkedColorElement(
+        lst[0], lst[1], lst[2],
+        $("div.rgbMixer.mixerDisplayLeft").get(0)
+    ); rgbMixColor1.alpha = 0.67;
+
+    var rgbMixColor2 = new LinkedColorElement(
+        lst[3], lst[4], lst[5],
+        $("div.rgbMixer.mixerDisplayRight").get(0)
+    ); rgbMixColor2.alpha = 0.67;
+
+    $("input.rgbMixer").on("input", function() {
+        var mixEl = $("div.rgbMixer.mixerDisplayCenter").get(0);
+        var bias = 0; var c1, c2;
+        if ($(this).attr("type") == "number") {
+            bias = $(this).val();
+            bias = bias > 100 ? 100 : bias < 0 ? 0 : bias;
+            $("input.bias-slider.rgbMixer").val(bias);
+        } else if ($(this).attr("class") == "bias-slider rgbMixer") {
+            bias = $(this).val();
+            $("input.bias-num.rgbMixer").val(bias);
+        } else bias = $("input.bias-slider.rgbMixer").val();
+        if ($(this).attr("type") == "color") {
+            if ($(this).attr("data-color-id") == "1") {
+                c1 = $(this).val();
+                c2 = rgbMixColor2.getColor().hex;
+            } else {
+                c1 = rgbMixColor1.getColor().hex;
+                c2 = $(this).val();
+            }
+        } else {
+            c1 = rgbMixColor1.getColor();
+            c2 = rgbMixColor2.getColor();
+        }
+        if ($("#UseHSL").val() === "1") {
+            var mixc = ColorHSL.mix(c1, c2, bias/100, false); mixc.a = 0.67;
+            $(".hslMixer").get(0).style.backgroundColor = mixc.formatHSL;
+        }
+
+        mix = ColorRGB.mix(c1, c2, bias/100, false); mix.a = 0.67;
+        mixEl.style.backgroundColor = mix.formatRGB;
+    });
+    $(".mixerDisplayCenter1, .bias-slider").css("width","300px");
+    var c = ColorRGB.mix(rgbMixColor1.getColor(), rgbMixColor2.getColor(), 0.5, false); c.alpha = 0.85;
+    a3 = $(".rgbMixer.mixerDisplayCenter");
+    a3.get(0).style.backgroundColor = c.formatRGB;
+    $(".mixerDisplayCenter").css("height", "87px");
+    $("div.hslMixer").css({
+        "border-top": "none",
+        "border-bottom": "1px solid #444",
+        "border-top-left-radius": "1%",
+        "border-top-right-radius": "1%",
+        "border-bottom-left-radius": "15%",
+        "border-bottom-right-radius": "15%"
+
+    });
+
+    $("div.hslMixer").hide();
+    $(".rgbMixer.mixerDisplayCenter").css("height", "175px");
+
+    $("button.rgbMixer").click(function() {
+        UseHSL.value = UseHSL.value == 1 ? 0 : 1;
+        if (UseHSL.value == 1) {
+            $(this).text("Hide HSL Mix");
+            $("div.hslMixer").show("slow");
+            $(".rgbMixer.mixerDisplayCenter").css({
+                "height": "87px",
+                "border-bottom": "none",
+                "border-bottom-left-radius": "1%",
+                "border-bottom-right-radius": "1%"
+            });
+        } else {
+            $(this).text("Show HSL Mix");
+            $(".rgbMixer.mixerDisplayCenter").css({
+                "height": "175px",
+                "border-bottom": "1px solid #000",
+                "border-bottom-left-radius": "15%",
+                "border-bottom-right-radius": "15%"
+            });
+            $("div.hslMixer").hide("slow");
+        }
+    });
+})();
