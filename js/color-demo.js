a11b16746635cc6cb0048cd8b3675a5f8dec1042
brenton-h <37673694+brenton-h@users.noreply.github.com>
2018-11-21 21:42:01 -0500

    Elaborated Project

--- js/color-demo.js
+++ js/color-demo.js
@@ -2,9 +2,9 @@
 
     $("input.R2G").on("input", function() {
         var clr = jsrc.color.redToGreenRatio($(this).val() / 100, false);
-        let fc = "rgba("+clr[0]+","+clr[1]+","+clr[2]+","+0.67+")";
+        let fc = "rgba("+clr[0]+","+clr[1]+","+clr[2]+","+0.75+")";
         $("div.R2G").css("background-color", fc);
-    });
+    }); var UseHSL = false;
 
     function checkBrightness(R, G, B) {
         R /= 255; G /= 255; B /= 255;
@@ -17,12 +17,12 @@
     var rgbMixColor1 = new LinkedColorElement(
         lst[0], lst[1], lst[2],
         $("div.rgbMixer.mixerDisplayLeft").get(0)
-    ); rgbMixColor1.alpha = 0.67;
+    ); rgbMixColor1.alpha = 0.75;
 
     var rgbMixColor2 = new LinkedColorElement(
         lst[3], lst[4], lst[5],
         $("div.rgbMixer.mixerDisplayRight").get(0)
-    ); rgbMixColor2.alpha = 0.67;
+    ); rgbMixColor2.alpha = 0.75;
 
     $("input.rgbMixer").on("input", function() {
         var mixEl = $("div.rgbMixer.mixerDisplayCenter").get(0);
@@ -37,9 +37,23 @@
         } else bias = $("input.bias-slider.rgbMixer").val();
         if ($(this).attr("type") == "color") {
             if ($(this).attr("data-color-id") == "1") {
+                var cc = new ColorRGB(0,0,0, rgbMixColor1.alpha);
+                cc.hex = $(this).val();
+                rgbMixColor1.r = cc.r;
+                rgbMixColor1.g = cc.g;
+                rgbMixColor1.b = cc.b;
+                rgbMixColor1.alpha = cc.a;
+                LinkedColorElement.UpdateColor(rgbMixColor1);
                 c1 = $(this).val();
                 c2 = rgbMixColor2.getColor().hex;
             } else {
+                var cc = new ColorRGB(0,0,0, rgbMixColor2.alpha);
+                cc.hex = $(this).val();
+                rgbMixColor2.r = cc.r;
+                rgbMixColor2.g = cc.g;
+                rgbMixColor2.b = cc.b;
+                rgbMixColor2.alpha = cc.a;
+                LinkedColorElement.UpdateColor(rgbMixColor2);
                 c1 = rgbMixColor1.getColor().hex;
                 c2 = $(this).val();
             }
@@ -47,16 +61,26 @@
             c1 = rgbMixColor1.getColor();
             c2 = rgbMixColor2.getColor();
         }
-        if ($("#UseHSL").val() === "1") {
-            var mixc = ColorHSL.mix(c1, c2, bias/100, false); mixc.a = 0.67;
+        if (UseHSL) {
+            var mixc = ColorHSL.mix(c1, c2, bias/100, false); mixc.a = 0.75;
             $(".hslMixer").get(0).style.backgroundColor = mixc.formatHSL;
         }
 
-        mix = ColorRGB.mix(c1, c2, bias/100, false); mix.a = 0.67;
+        mix = ColorRGB.mix(c1, c2, bias/100, false); mix.a = 0.75;
         mixEl.style.backgroundColor = mix.formatRGB;
     });
+
+    rgbMixColor1.r = 255;
+    rgbMixColor1.g = 128;
+    rgbMixColor1.b = 192;
+    LinkedColorElement.UpdateColor(rgbMixColor1);
+    rgbMixColor2.r = 255;
+    rgbMixColor2.g = 128;
+    rgbMixColor2.b = 0;
+    LinkedColorElement.UpdateColor(rgbMixColor2);
+
     $(".mixerDisplayCenter1, .bias-slider").css("width","300px");
-    var c = ColorRGB.mix(rgbMixColor1.getColor(), rgbMixColor2.getColor(), 0.5, false); c.alpha = 0.85;
+    var c = ColorRGB.mix(rgbMixColor1.getColor(), rgbMixColor2.getColor(), 0.5, false); c.alpha = 0.75;
     a3 = $(".rgbMixer.mixerDisplayCenter");
     a3.get(0).style.backgroundColor = c.formatRGB;
     $(".mixerDisplayCenter").css("height", "87px");
@@ -73,17 +97,18 @@
     $("div.hslMixer").hide();
     $(".rgbMixer.mixerDisplayCenter").css("height", "175px");
 
-    $("button.rgbMixer").click(function() {
-        UseHSL.value = UseHSL.value == 1 ? 0 : 1;
-        if (UseHSL.value == 1) {
+    $("button.rgbMixer").on("click", function() {
+        UseHSL = !UseHSL;
+        //$("UseHSL").val(($("UseHSL").val() == "1" ? "0" : "1"));
+        if (UseHSL) {
             $(this).text("Hide HSL Mix");
-            $("div.hslMixer").show("slow");
             $(".rgbMixer.mixerDisplayCenter").css({
                 "height": "87px",
                 "border-bottom": "none",
                 "border-bottom-left-radius": "1%",
                 "border-bottom-right-radius": "1%"
             });
+            $("div.hslMixer").show();
         } else {
             $(this).text("Show HSL Mix");
             $(".rgbMixer.mixerDisplayCenter").css({
@@ -92,7 +117,7 @@
                 "border-bottom-left-radius": "15%",
                 "border-bottom-right-radius": "15%"
             });
-            $("div.hslMixer").hide("slow");
+            $("div.hslMixer").hide();
         }
     });
 })();
